182654c0b6f5c0bf2afe4e53084941986c80154d
jaryP <jarypomponi@gmail.com>
2025-07-22 16:36:58 +0200

    Slurm files modified

--- slurm/ablations/batch_size.sh
+++ slurm/ablations/batch_size.sh
@@ -3,11 +3,12 @@
 model=${1:-"deit_tiny_patch16_224"}
 dataset=${2:-"cifar_100"}
 compression=${3:-0.5}
+method=${4:-"proposal"}
 
 #models=("deit_tiny_patch16_224" "deit_small_patch16_224")
 #datasets=("cifar_100" "food_101")
 
 pool=(16 32 64 128)
 for ppl in "${pool[@]}"; do
-    sbatch  slurm/main_batch_size.sh "proposal" $model $dataset $compression $ppl
+    sbatch  slurm/main_batch_size.sh $method $model $dataset $compression $ppl
 done
