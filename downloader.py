7dd6426f0af1f6455ecb73d6ad83cfce2caf347d
jaryP <jarypomponi@gmail.com>
2025-09-04 15:22:15 +0200

    imagenette added

--- downloader.py
+++ downloader.py
@@ -9,8 +9,8 @@ torchvision.datasets.CIFAR100('./data', train=False, download=True)
 torchvision.datasets.Food101('./data', split='train', download=True)
 torchvision.datasets.Food101('./data', split='test', download=True)
 
-torchvision.datasets.Imagenette('./data', split='train', download=True, size='320')
-torchvision.datasets.Imagenette('./data', split='test', download=True, size='320')
+torchvision.datasets.Imagenette('./data', split='train', download=True, size='full')
+torchvision.datasets.Imagenette('./data', split='test', download=True, size='full')
 
 # MODELS
 timm.create_model(model_name='deit_small_patch16_224.fb_in1k', pretrained=True)
