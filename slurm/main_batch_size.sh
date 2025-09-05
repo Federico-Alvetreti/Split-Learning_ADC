60bfdad2171d551dad93d47dee82f111eea3f127
jaryP <jarypomponi@gmail.com>
2025-09-05 14:40:05 +0200

    debug in sh file

--- slurm/main_batch_size.sh
+++ slurm/main_batch_size.sh
@@ -66,7 +66,7 @@ if [ ${1} = 'proposal' ]; then
 elif [ ${1} = 'base' ]; then
   srun python main.py method=${1} dataset=${3} model=${2} hydra=batch_ablation dataset.batch_size=${5}
 elif [ ${1} = 'c3-sl' ]; then
-  srun python main.py method=${1} dataset=${3} model=${2} method.parameters.R=${4}
+  srun python main.py method=${1} dataset=${3} model=${2} method.parameters.R=${4} hydra=batch_ablation
 else
 	echo "ERROR: method not recognized"
   exit 1
